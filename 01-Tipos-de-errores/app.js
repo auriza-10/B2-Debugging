91afc881bbf8b1c3b74723be3637e1327a91b9b7
Andrew Alvarado <andrew@activetheory.net>
2025-10-15 19:56:40 -0600

    Teoria 01

--- 01-Tipos-de-errores/app.js
+++ 01-Tipos-de-errores/app.js
@@ -1,1 +1,19 @@
-console.log('01. Tipos de errores o bugs.');
+/* 01. Tipos de errores o bugs. */
+
+// ❌ 1. Error de sintaxis:
+if (true { 
+  console.log("Hola");
+}
+
+// ❌ 2. Error de referencia:
+console.log(nombre);
+
+// ❌ 3. Error de tipo:
+let numero = 42;
+numero.toUpperCase(); // este método solo existe en Strings
+
+// ❌ 4. Error lógico:
+let a = 10;
+let b = 5;
+let promedio = a + b / 2; // el promedio debería ser (a + b) / 2
+console.log(promedio); // Resultado incorrecto: 12.5
